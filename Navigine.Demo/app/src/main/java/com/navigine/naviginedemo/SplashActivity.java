4c04d5132bb52f630a897c02abeb06631b529d04
Fedor Puchkov <Fedor.Puchkov@navigine.com>
2016-10-21 13:39:18 +0300

    Added timeout in loading location.

--- Navigine.Demo/app/src/main/java/com/navigine/naviginedemo/SplashActivity.java
+++ Navigine.Demo/app/src/main/java/com/navigine/naviginedemo/SplashActivity.java
@@ -15,6 +15,7 @@ import com.navigine.naviginesdk.*;
 public class SplashActivity extends Activity
 {
   private static final String TAG = "Navigine.Demo";
+  private static final int LOADER_TIMEOUT = 30; // seconds
   
   private TextView  mErrorLabel   = null;
   private TimerTask mTimerTask    = null;
@@ -52,34 +53,57 @@ public class SplashActivity extends Activity
     moveTaskToBack(true);
   }
   
-  private int mLoader = 0;
+  private int  mLoader       = 0;
+  private int  mLoaderState  = 0;
+  private long mLoaderTime   = 0;
+  
   private void updateLoader()
   {
     if (DemoApp.Navigation == null)
       return;
-
+    
+    long timeNow = NavigineSDK.currentTimeMillis() / 1000;
+    
     if (mLoader == 0)
     {
-      mLoader = NavigineSDK.startLocationLoader(DemoApp.LOCATION_NAME, false);
+      mLoader      = NavigineSDK.startLocationLoader(DemoApp.LOCATION_NAME, false);
+      mLoaderTime  = timeNow;
+      mLoaderState = 0;
     }
     else
     {
       int state = NavigineSDK.checkLoader(mLoader);
       if (state >= 0 && state <= 99)
       {
+        if (Math.abs(timeNow - mLoaderTime) > LOADER_TIMEOUT)
+        {
+          NavigineSDK.stopLoader(mLoader);
+          mLoader      = 0;
+          mLoaderTime  = 0;
+          mLoaderState = 0;
+          Log.d(TAG, "Location loader: TIMEOUT");
+          return;
+        }
+        if (mLoaderState != state)
+        {
+          mLoaderTime  = timeNow;
+          mLoaderState = state;
+        }
         Log.d(TAG, "Location loader: progress " + state + "%");
         return;
       }
       NavigineSDK.stopLoader(mLoader);
-      mLoader = 0;
-
+      mLoader      = 0;
+      mLoaderTime  = 0;
+      mLoaderState = 0;
+      
       if (state == 100)
         Log.d(TAG, "Location loader: FINISHED");
       else
         Log.d(TAG, "Location loader: FAILED with error=" + state);
     }
   }
-
+  
   boolean mInitialized = false;
   boolean mFinished    = false;
   
